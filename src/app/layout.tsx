e664f5f34d3241dd99f97211d2e30aa9f36a9d9b
MelAlejandrino <alejandrinomel02@gmail.com>
2026-03-26 08:16:54 +0800

    feat: metadata on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,23 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: 'jeje/ui',
+  description:
+    'Practical, copy-paste React components built on top of shadcn/ui. Designed for dashboards, admin panels, and internal tools — but flexible enough for any project.',
+  openGraph: {
+    title: 'jeje/ui',
+    description:
+      'Practical, copy-paste React components built on top of shadcn/ui. Designed for dashboards, admin panels, and internal tools — but flexible enough for any project.',
+    images: '/icon.jpg',
+  },
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
